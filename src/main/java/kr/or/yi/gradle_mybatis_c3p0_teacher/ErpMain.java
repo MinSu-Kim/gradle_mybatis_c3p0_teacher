d132d87b91990c9369c98a6c0621690a72927424
영남인재교사 <net94.teacher@gmail.com>
2019-07-22 17:59:59 +0900

    Look And Feel 추가

--- src/main/java/kr/or/yi/gradle_mybatis_c3p0_teacher/ErpMain.java
+++ src/main/java/kr/or/yi/gradle_mybatis_c3p0_teacher/ErpMain.java
@@ -8,9 +8,18 @@ public class ErpMain {
 
 	public static void main(String[] args) {
 		try {
-			// select Look and Feel
-			UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
-			// start application
+			// JTattoo.jar : JTattoo Look and Feel
+//			UIManager.setLookAndFeel("com.jtattoo.plaf.smart.SmartLookAndFeel");
+//			UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
+			// SeaGlassLookAndFeel
+//			UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+			// 2. quaqua.jar : Quaqua Look and Feel
+//			UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
+			// 3. liquidlnf.jar : Liquid Look and Feel
+//			UIManager.setLookAndFeel("com.birosoft.liquid.LiquidLookAndFeel");
+			// 4. idw-gpl.jar : InfoNode Look and Feel
+//			UIManager.setLookAndFeel("net.infonode.gui.laf.InfoNodeLookAndFeel");
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
